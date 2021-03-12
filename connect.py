eabbd128f26f6c985a866d160ebeb74501327202
Michał Sobieraj <michalsobieraj966@wp.pl>
2021-03-12 14:29:01 +0100

    Printing out info about db connection

--- connect.py
+++ connect.py
@@ -5,16 +5,21 @@ def connect():
     db_config = RawConfigParser()
     db_config.read('connection.properties')
     db_config = db_config['DEFAULT']
-    return psycopg2.connect(
+    connection = psycopg2.connect(
     host=db_config.get('host'),
     port=db_config.get('port'),
     dbname=db_config.get('dbname'),
     user=db_config.get('user'),
     password=db_config.get('password'))
+    if connection != None:
+        print("Connection successfully instantiated to host",
+         db_config.get('host'), "and database", db_config.get('dbname'))
+    return connection
 
 
 def main():
     db_conn = connect()
+
 
 
 if __name__ == '__main__':
